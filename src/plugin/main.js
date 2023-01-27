eacd9d061a3d9262b65f23d927727c6d09e379b3
corentinleberre <corentin.leb29@gmail.com>
2023-01-27 14:08:16 -0500

    :art: Improve structure

--- src/plugin/main.js
+++ src/plugin/main.js
@@ -2,6 +2,14 @@ import { editOnGitPlugin, customFooterPlugin } from "./my-plugin";
 
 const docsify = window.$docsify || {};
 
-const props = { editOnGitPlugin: docsify.editOnGitPlugin, customFooterPlugin: docsify.customFooterPlugin } || {};
+const props =
+  {
+    editOnGitPlugin: docsify.editOnGitPlugin,
+    customFooterPlugin: docsify.customFooterPlugin,
+  } || {};
 
-docsify.plugins = [].concat(docsify.plugins || [], editOnGitPlugin(props.editOnGitPlugin), customFooterPlugin(props.customFooterPlugin));
+docsify.plugins = [].concat(
+  docsify.plugins || [],
+  editOnGitPlugin(props.editOnGitPlugin),
+  customFooterPlugin(props.customFooterPlugin)
+);
